ca9fb6c391658a13c822075cf4f86a74f7e0fd1f
Giorgio Pomettini <giorgio.pomettini@gmail.com>
2018-08-27 21:51:51 +0200

    Small refactor

--- main.c
+++ main.c
@@ -1,46 +1,38 @@
 #include "main.h"
 
-// Thanks to purplealienplanet.com
-
-typedef struct
-{
-	float x, y, z;
-} vector;
-
-typedef struct
-{
-	vector pos;
-	float radius;
-} sphere;
-
-typedef struct
-{
-	vector start;
-	vector dir;
-} ray;
-
-vector vector_sub(vector *v1, vector *v2)
+static inline vector vector_sub(vector *v1, vector *v2)
 {
 	vector result = { v1->x - v2->x, v1->y - v2->y, v1->z - v2->z };
 	return result;
 }
 
-float vector_dot(vector *v1, vector *v2)
+static inline float vector_dot(vector *v1, vector *v2)
 {
 	return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
 }
 
-int intersect_ray_sphere(ray *r, sphere *s)
+static inline int intersect_ray_sphere(ray *r, sphere *s)
 {
 	float a = vector_dot(&r->dir, &r->dir);
 	vector dist = vector_sub(&r->start, &s->pos);
 	float b = 2 * vector_dot(&r->dir, &dist);
 	float c = vector_dot(&dist, &dist) - (s->radius * s->radius);
 	float discr = b * b - 4 * a * c;
+
 	if (discr < 0)
 		return 0;
 	else
 		return 1;
+}
+
+static inline unsigned short rgb(int r, int g, int b)
+{
+	return r | (g << 5) | (b << 10);
+}
+
+static inline void put_pixel(int x, int y, short color)
+{
+	((unsigned short*)0x06000000)[y * SCREEN_WIDTH + x] = color;
 }
 
 int main()
@@ -81,8 +73,10 @@ int main()
 		for (x = 0; x < SCREEN_WIDTH; x++)
 		{
 			r.start.x = x;
+			
 			hit1 = intersect_ray_sphere(&r, &s1);
 			hit2 = intersect_ray_sphere(&r, &s2);
+			
 			if (hit2)
 			{
 				put_pixel(x, y, rgb(255, 255, 0));
@@ -97,14 +91,4 @@ int main()
 	while(1);
 
 	return 0;
-}
-
-static inline unsigned short rgb(int r, int g, int b)
-{
-	return r | (g << 5) | (b << 10);
-}
-
-static inline void put_pixel(int x, int y, short color)
-{
-	((unsigned short*)0x06000000)[y * SCREEN_WIDTH + x] = color;
 }
--- main.h
+++ main.h
@@ -4,6 +4,29 @@
 #define SCREEN_WIDTH 240
 #define SCREEN_HEIGHT 160
 
+// Thanks to purplealienplanet.com
+
+typedef struct
+{
+	float x, y, z;
+} vector;
+
+typedef struct
+{
+	vector pos;
+	float radius;
+} sphere;
+
+typedef struct
+{
+	vector start;
+	vector dir;
+} ray;
+
+static inline vector vector_sub(vector *v1, vector *v2);
+static inline float vector_dot(vector *v1, vector *v2);
+static inline int intersect_ray_sphere(ray *r, sphere *s);
+
 static inline void put_pixel(int x, int y, short color);
 static inline unsigned short rgb(int r, int g, int b);
 
